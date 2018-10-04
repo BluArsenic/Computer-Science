fb72a2f63408dba5e116f69ca73fd4459d8a2304
BluArsenic <yelloarsenic@gmail.com>
2018-10-04 00:56:28 -0400

    working version

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,11 +1,58 @@
 import random
 
-w = int(input("Width: "))
-h = int(input("Height: "))
+w = 2 + int(input("Width: "))
+# h = 2 + int(input("Height: "))
 b = int(input("Bombs: "))
-m = []
-for x in range(w):
-	m.append(0)
-for x in range(h):
-	m[random.randint(0,w-1)] = "B"
-	print(*m)
+m = [[0]*w for x in range(w)]
+for x in range(b):
+	a = random.randrange(1,w-1)
+	b = random.randrange(1,w-1)
+	m[a][b] = "b"
+	if m[a-1][b] is not "b":
+		m[a-1][b] +=1
+	else:
+		m[a-1][b] == "b"
+	if m[a-1][b-1] is not "b":
+		m[a-1][b-1] +=1
+	else:
+		m[a-1][b-1] == "b"
+	if m[a-1][b+1] is not "b":
+		m[a-1][b+1] +=1
+	else:
+		m[a-1][b+1] == "b"
+	if m[a][b+1] is not "b":
+		m[a][b+1] +=1
+	else:
+		m[a][b-1] == "b"
+	if m[a][b-1] is not "b":
+		m[a][b-1] +=1
+	else:
+		m[a][b+1] == "b"
+	if m[a+1][b] is not "b":
+		m[a+1][b] +=1
+	else:
+		m[a+1][b] == "b"
+	if m[a+1][b+1] is not "b":
+		m[a+1][b+1] +=1
+	else:
+		m[a+1][b+1] == "b"
+	if m[a+1][b-1] is not "b":
+		m[a+1][b-1] +=1
+	else:
+		m[a+1][b-1] == "b"
+	# if m[a][b] == "b":
+	# 	m[a-1][b] +=1
+	# 	m[a-1][b-1] +=1
+	# 	m[a-1][b+1] +=1
+	# 	m[a][b-1] +=1
+	# 	m[a][b+1] +=1
+	# 	m[a+1][b] +=1
+	# 	m[a+1][b-1] +=1
+	# 	m[a+1][b+1] +=1
+del m[0]
+del m[w-2]
+for x in range(w-2):
+	del m[x][0]
+	del m[x][w-2]
+for x in m:
+	print(*x)
