5c58abfefd6ef9ef8b11aa9e2f8afc49a94625e6
BluArsenic <yelloarsenic@gmail.com>
2018-11-05 23:15:28 -0500

    Card and Deck
    Card and deck class (card is one card, deck is an entire deck)

---
+++ cards.py
@@ -0,0 +1,66 @@
+import random
+
+class Card:
+	def __init__(self):
+		self.deck = []	
+	def __str__(self):
+		deck = self.deck
+		status = ""
+		for i in range(1,14):
+			for j in range(1,5):
+				if i is 1:
+					i = "Ace"
+				elif i is 11:
+					i = "Jack"
+				elif i is 12:
+					i = "Queen"
+				elif i is 13:
+					i = "King"
+				if j is 1:
+					j = "Clubs"
+				elif j is 2:
+					j = "Diamonds"
+				elif j is 3:
+					j = "Hearts"
+				elif j is 4:
+					j = "Spades"
+				deck.append((i,j))
+		x = random.randrange(len(deck))
+		status = "\n"+str(deck[x][0])+" of "+str(deck[x][1])
+		return status		
+
+class Deck:
+	def __init__(self):
+		self.deck = []	
+	def __str__(self):
+		deck = self.deck
+		status = ""
+		for i in range(1,14):
+			for j in range(1,5):
+				if i is 1:
+					i = "Ace"
+				elif i is 11:
+					i = "Jack"
+				elif i is 12:
+					i = "Queen"
+				elif i is 13:
+					i = "King"
+				if j is 1:
+					j = "Clubs"
+				elif j is 2:
+					j = "Diamonds"
+				elif j is 3:
+					j = "Hearts"
+				elif j is 4:
+					j = "Spades"
+				deck.append((i,j))
+		x = random.randrange(len(deck))
+		status = "\n"+str(deck[x][0])+" of "+str(deck[x][1])
+		return status		
+		# for x in deck:
+		# 	status += "\n"+str(x[0])+" of "+str(x[1])
+		# return status
+		# print(str(x[0])+" of "+str(x[1]))
+		# print(str(i)+" of "+str(j))
+
+print(Card())
