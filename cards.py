c347e90ca9ca370a3aff75c68758f6f0a77a50de
BluArsenic <yelloarsenic@gmail.com>
2018-11-06 09:05:01 -0500

    ?

--- cards.py
+++ cards.py
@@ -27,11 +27,11 @@ class Card:
 				card.append((i,j))
 		x = random.randrange(len(card))
 		status = "\n"+str(card[x][0])+" of "+str(card[x][1])
-		return status		
+		return status
 
 class Deck:
 	def __init__(self):
-		self.deck = []	
+		self.deck = []
 	def __str__(self):
 		deck = self.deck
 		status = ""
@@ -58,8 +58,6 @@ class Deck:
 		for x in range(len(deck)):
 			status += "\n"+str(deck[x][0])+" of "+str(deck[x][1])
 		return status
-		# print(str(x[0])+" of "+str(x[1]))
-		# print(str(i)+" of "+str(j))
 
 print(Card())
 print(Deck())
