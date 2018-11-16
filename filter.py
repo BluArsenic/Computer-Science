d7cd7423c78c2fbb3426206d04e8f961ccfbf906
BluArsenic <yelloarsenic@gmail.com>
2018-11-16 14:09:05 -0500

    Unfinished filter

--- filter.py
+++ filter.py
@@ -1,14 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 imgx = 256
-image = Image.new('RGBA',(imgx,imgx))
-
-for x in range(imgx):
-	for y in range(imgx):
-		image.putpixel((x,y),(255,0,0,0))
+image = Image.new('RGBA',(imgx,imgx), (255))
 
 for x in range(imgx):
 	for y in range(imgx):
 		image.putpixel((x,y),(0,255,0,0))
 
-image.show("test.png", "PNG")
+Image.blend(image, image, .3).show("test.png", "PNG")
