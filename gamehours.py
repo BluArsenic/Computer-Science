2f74d4705614405572c609debe5486aaf7287bcb
BluArsenic <yelloarsenic@gmail.com>
2019-01-15 12:12:50 -0500

    Project idea

---
+++ gamehours.py
@@ -0,0 +1,11 @@
+'''
+How many days can you get on electronics per week?
+	-1-7 days
+How many hours can you spend per day on electronics?
+	-Depends on sleep, classes, and extra activites
+	-Most hours:
+	-Least hours (8 hrs. sleep, 7 hour school day, 1 hour getting ready, 2 hours sports:
+How many different things can you do on electronics?
+
+ok um think about this one later
+'''
