80b1f695338b4330bf72a872ea5798b8d2b11ff8
BluArsenic <yelloarsenic@gmail.com>
2019-01-18 15:10:08 -0500

    Update TurtleProject.py

--- TurtleProject.py
+++ TurtleProject.py
@@ -1,1 +1,18 @@
-TurtleProject
+#Function = Method, but Method != Function
+#Draw
+#The Turtle's Postions
+#Measurement
+#Drawing State
+#Color
+#Fill(ing)
+#Drawing Control
+#Turtle Visibility
+#Turtle Appearance
+#Using Events (?)
+#Special Turtle Fuctions/Methods
+#Window Control
+#Animation Control
+#Using Screen Events (?)
+#Settings (and special methods?)
+#Input Functions (?)
+#Screen-Specific Methods
