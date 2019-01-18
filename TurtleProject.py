35596e45caf35c07e64190dcb025c889311f438a
BluArsenic <yelloarsenic@gmail.com>
2019-01-18 15:21:28 -0500

    Added one definition and example

--- TurtleProject.py
+++ TurtleProject.py
@@ -1,5 +1,14 @@
 #Function = Method, but Method != Function
+
+
 #Draw
+
+# Moves the "turtle" forward by a specified distance in the direction it's facing. You can use either method.
+# In this example, the turtle moves forward 100 steps
+distance = 100
+turtle.forward(distance)
+turtle.fd(distance)
+
 #The Turtle's Postions
 #Measurement
 #Drawing State
