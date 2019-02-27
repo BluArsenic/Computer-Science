f4f0381461eeeb1b89017d634ecbf77137fcd9dd
BluArsenic <yelloarsenic@gmail.com>
2019-02-27 09:50:22 -0500

    Added multiple graphs and description

--- laundrytimes.py
+++ laundrytimes.py
@@ -1,4 +1,16 @@
 '''
+When Mrs. Healey told us about the project, I first thought it would be fun to see how many hours I spend on my phone!
+Then I thought, "Well, it won't be that useful because I can just look at my phone."
+Next, I thought it would be a cool idea to track how much time it takes on average to do my homework.
+I didn't think about that one for long because the next thought I had was the resulting code.
+I decided to track, on average, the day that kids wash their clothes the most in Tenney.
+I have a function that calculates the days that factors in the kids that use E&R, and one that doesn't.
+I dont have the one with E&R activated because it heavily skews the results.
+It prints multiple graphs of multiples weeks  of washing, and you can change the number of weeks in the weeks variable to procude more graphs and data.
+Hopefully this can be helpful to me in the future!
+'''
+
+'''
 What days of the week can you wash your laundry?
 	-All days
 What times can you wash them?
@@ -65,45 +77,49 @@ def calculateNoER():
 	dataFri = [] # data of friday washes
 	dataSat = [] # data of saturday washes
 	dataSun = [] # data of sunday washes
+	dataWeeks = []
+	weeks = 5
 	colors = r.choice(['red','orange','yellow','green','blue','purple','magenta','cyan'])
 	days = ["Mon.","Tues.","Wed.","Thurs.","Fri.","Sat.","Sun."]
+	weekDays = [days for x in range(weeks)]
 	daysResults = []
-	# for x in range(weeks): # if you want to implement weeks
 	students = 40 # students in the dorm; 40 for reference (Tenney population)
-	for washed in range(students):
-		whichDay() # gets value for day
-		whichTime() # gets vaule for time of day
-		ER() # gets value of E&R use
-		if er == 'No' or er == 'Sometimes':
-			if day == 'Monday':
-				dataMon.append(time)
-			elif day == 'Tuesday':
-				dataTues.append(time)
-			elif day == 'Wednesday':
-				dataWed.append(time)
-			elif day == 'Thursday':
-				dataThurs.append(time)
-			elif day == 'Friday':
-				dataFri.append(time)
-			elif day == 'Saturday':
-				dataSat.append(time)
-			else:
-				dataSun.append(time)
-		washed += 1
-	daysResults.append(len(dataMon))
-	daysResults.append(len(dataTues))
-	daysResults.append(len(dataWed))
-	daysResults.append(len(dataThurs))
-	daysResults.append(len(dataFri))
-	daysResults.append(len(dataSat))
-	daysResults.append(len(dataSun))
-	plt.bar(days, daysResults, .8, color = colors)
-	plt.title("Days Students W/O E&R Wash Their Clothes in Tenney")
-	plt.savefig("laundry1.png")
+	for x in range(weeks): # if you want to implement weeks
+		for washed in range(students):
+			whichDay() # gets value for day
+			whichTime() # gets vaule for time of day
+			ER() # gets value of E&R use
+			if er == 'No' or er == 'Sometimes':
+				if day == 'Monday':
+					dataMon.append(time)
+				elif day == 'Tuesday':
+					dataTues.append(time)
+				elif day == 'Wednesday':
+					dataWed.append(time)
+				elif day == 'Thursday':
+					dataThurs.append(time)
+				elif day == 'Friday':
+					dataFri.append(time)
+				elif day == 'Saturday':
+					dataSat.append(time)
+				else:
+					dataSun.append(time)
+			washed += 1
+		daysResults.append(len(dataMon))
+		daysResults.append(len(dataTues))
+		daysResults.append(len(dataWed))
+		daysResults.append(len(dataThurs))
+		daysResults.append(len(dataFri))
+		daysResults.append(len(dataSat))
+		daysResults.append(len(dataSun))
+		dataWeeks.append(daysResults)
+		daysResults = []
+	for x in range(weeks):
+		plt.figure(x+1)
+		plt.bar(weekDays[x], dataWeeks[x], .8, color = colors)
+		plt.title("Days Students W/O E&R Wash Their Clothes in Tenney: Week "+str(x+1))
 	plt.show()
-	return daysResults,len(dataMon),len(dataTues),len(dataWed),len(dataThurs),len(dataFri),len(dataSat),len(dataSun)
 
-# weeks = 10
 print(calculateNoER())
 
 # def calculateER(): # skews the results with the E&R results; if you want to add it back in, you can. That's why I'm leaving it commented.
